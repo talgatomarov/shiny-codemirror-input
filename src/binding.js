ef61872b5b36bd6a9057a866ffc7121a7e8cc1e0
talgatomarov <talgat.omarov.1999@gmail.com>
2024-09-02 13:26:17 -0600

    Remove console.log statements

--- src/binding.js
+++ src/binding.js
@@ -1,45 +1,39 @@
-import { EditorView, basicSetup } from "codemirror"
-import { EditorState } from "@codemirror/state"
-import { sql } from "@codemirror/lang-sql"
+import { EditorView, basicSetup } from "codemirror";
+import { EditorState } from "@codemirror/state";
+import { sql } from "@codemirror/lang-sql";
 
 class CodeMirrorInputBinding extends Shiny.InputBinding {
-    initialize(el) {
-      this.editor = null;
-    }
-
-    find(scope) {
-      return $(scope).find(".codemirror-input");
-    }
-  
-    getValue(el) {
-      if (this.editor === null) return "";
-      console.log(this.editor.state.doc.toString());
-      return this.editor.state.doc.toString();
-    }
-
-    subscribe(el, callback) {
-      const state = EditorState.create({
-        extensions: [
-            basicSetup, 
-            sql(),
-            EditorView.updateListener.of((update) => {
-              if (update.docChanged) {
-                console.log("TEST");
-                callback();
-              }
-            })
-        ]
-      });
-
-      this.editor = new EditorView({
-          state: state,
-          parent: el
-      })
-    }
+  initialize(el) {
+    this.editor = null;
   }
-  
-  Shiny.inputBindings.register(new CodeMirrorInputBinding(), "codemirror-input");
 
+  find(scope) {
+    return $(scope).find(".codemirror-input");
+  }
 
+  getValue(el) {
+    if (this.editor === null) return "";
+    return this.editor.state.doc.toString();
+  }
 
+  subscribe(el, callback) {
+    const state = EditorState.create({
+      extensions: [
+        basicSetup,
+        sql(),
+        EditorView.updateListener.of((update) => {
+          if (update.docChanged) {
+            callback();
+          }
+        }),
+      ],
+    });
 
+    this.editor = new EditorView({
+      state: state,
+      parent: el,
+    });
+  }
+}
+
+Shiny.inputBindings.register(new CodeMirrorInputBinding(), "codemirror-input");
